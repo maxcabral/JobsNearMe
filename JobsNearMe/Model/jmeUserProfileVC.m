98b3c004e838218b0a8d15caa2830e8f3f24dd66
Maxwell Cabral <max@maxcabral.com>
2013-06-01 22:35:38 -0700

    Changes

---
+++ JobsNearMe/Model/jmeUserProfileVC.m
@@ -0,0 +1,94 @@
+//
+//  jmeUserProfileVC.m
+//  JobsNearMe
+//
+//  Created by Maxwell Cabral on 6/1/13.
+//  Copyright (c) 2013 mCab. All rights reserved.
+//
+
+#import "jmeUserProfileVC.h"
+
+@interface jmeUserProfileVC ()
+{
+    IBOutlet UITextField *nameField;
+    IBOutlet UITextField *emailField;
+    IBOutlet UITextField *skillField;
+    IBOutlet UITableView *tView;
+    NSMutableArray *_store;
+}
+@end
+
+@implementation jmeUserProfileVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    UITapGestureRecognizer *touchToHide = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                  action:@selector(hideKeyboard)];
+    
+//    [self.view addGestureRecognizer:touchToHide];
+    
+    _store = [[NSMutableArray alloc] init];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)hideKeyboard
+{
+    [self.view endEditing:YES];
+}
+
+- (IBAction)addButtonTUI:(id)sender
+{
+    if (skillField.text.length > 0){
+        [_store addObject:skillField.text];
+        [tView reloadData];
+    }
+}
+
+#pragma TableViewDataSource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"skillCell"];
+    
+    cell.textLabel.text = [_store objectAtIndex:indexPath.row];
+    //[self configureCell:cell atIndexPath:indexPath];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _store.count;
+}
+
+#pragma TableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 35.0;
+}
+
+#pragma UItextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    [theTextField resignFirstResponder];
+    return YES;
+}
+
+@end
